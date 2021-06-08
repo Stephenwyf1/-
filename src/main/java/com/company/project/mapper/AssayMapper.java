f966212d1af709a416abc54555da9a3972ab7f0f
Yoyotao <409268784@qq.com>
2021-06-08 14:41:15 +0800

    初始化

---
+++ src/main/java/com/company/project/mapper/AssayMapper.java
@@ -0,0 +1,17 @@
+package com.company.project.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.company.project.entity.AssayEntity;
+
+/**
+ * <p>
+ * 化验 Mapper 接口
+ * </p>
+ *
+ * @author wyf
+ * @since 2021-06-06
+ */
+public interface AssayMapper extends BaseMapper<AssayEntity> {
+
+}
